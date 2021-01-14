c067ace5a9563d1dcd5e569fde13f81fd6795a99
austindickey <austindickey@me.com>
2021-01-14 17:30:08 -0600

    adding the logic for making the data call

--- assets/js/widget.js
+++ assets/js/widget.js
@@ -4,6 +4,24 @@ function getPlayerDetails() {
 
     console.log(platform)
     console.log(gamertag)
+
+    // settings for the call
+    const settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://call-of-duty-modern-warfare.p.rapidapi.com/warzone-matches/" + gamertag + "/" + platform,
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": "1d79a935fbmsh6ce840778fdfc27p1edc1fjsnfd7dbd504b08",
+            "x-rapidapi-host": "call-of-duty-modern-warfare.p.rapidapi.com"
+        }
+    }
+    
+    // making the call
+    $.ajax(settings).done(function (response) {
+        console.log(response)
+    })
+
 }
 
 $("#submit").on("click", function() {
